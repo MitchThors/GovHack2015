11f4c2bb4373a528c781c380cf85be28a8bf7b80
Shaun Hevey <shaun@hevey.co>
2015-07-04 00:04:05 +1000

    Added Articles

--- GovHack2015/GovHack2015/Controllers/HomeController.cs
+++ GovHack2015/GovHack2015/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GovHack2015.Logic;
 
 namespace GovHack2015.Controllers
 {
@@ -10,6 +11,8 @@ namespace GovHack2015.Controllers
     {
         public ActionResult Index()
         {
+            GetArticles a = new GetArticles();
+            var aa = a.PopulateArticles();
             return View();
         }
 
---
+++ GovHack2015/GovHack2015/Models/Article.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace GovHack2015.Models
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Date { get; set; }
+        [JsonProperty("Primary image")]
+        public string PrimaryImage { get; set; }
+        [JsonProperty("Primary image caption")]
+        public string PrimaryImageCaption { get; set; }
+        [JsonProperty("Primary image rights information")]
+        public string PrimaryImageRightsImage { get; set; }
+        public string Subjects { get; set; }
+        public string Station { get; set; }
+        public string State { get; set; }
+        public string Place { get; set; }
+        public string Keywords { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        [JsonProperty("MediaRSS URL")]
+        public string MediaRssUrl { get; set; }
+    }
+}
