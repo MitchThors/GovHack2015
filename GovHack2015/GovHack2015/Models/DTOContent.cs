4b8185ca573cfe60d60cff03f6067fbd8fae19cc
MitchThors <mitch.thors@gmail.com>
2015-07-04 17:44:05 +1000

    Revert "A better commmit.... Le sigh."
    This reverts commit a59e45f55a9dde0c88e96c94f9ea20347aa47052.

--- GovHack2015/GovHack2015/Models/DTOContent.cs
+++ GovHack2015/GovHack2015/Models/DTOContent.cs
@@ -8,6 +8,7 @@ namespace GovHack2015.Models
     public class DtoContent
     {
         public IEnumerable<IMarker> ArticleMarkerList { get; set; } 
+
         public IEnumerable<Article> ArticleList { get; set; } 
     }
 
