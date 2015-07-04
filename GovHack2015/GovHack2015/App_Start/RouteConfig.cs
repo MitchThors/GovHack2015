0bb33b63527fe4a181c754f155f51a4ee2c32039
MitchThors <mitch.thors@gmail.com>
2015-07-04 11:18:35 +1000

    Beginning the maps screen.

--- GovHack2015/GovHack2015/App_Start/RouteConfig.cs
+++ GovHack2015/GovHack2015/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using GovHack2015.Globals;
 
 namespace GovHack2015
 {
@@ -18,6 +19,10 @@ namespace GovHack2015
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            //  On application start, set API key as required.
+            var globalData = new GlobalData();
+            globalData.ConfigureApplication();
         }
     }
 }
---
+++ GovHack2015/GovHack2015/ViewModels/Map/mapGET.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GovHack2015.ViewModels.Map
+{
+    public class MapGET
+    {
+        //  Populate as needed.
+    }
+}
