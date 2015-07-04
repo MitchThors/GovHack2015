a59e45f55a9dde0c88e96c94f9ea20347aa47052
MitchThors <mitch.thors@gmail.com>
2015-07-04 17:39:16 +1000

    A better commmit.... Le sigh.

--- GovHack2015/GovHack2015/Models/DTOContent.cs
+++ GovHack2015/GovHack2015/Models/DTOContent.cs
@@ -8,7 +8,6 @@ namespace GovHack2015.Models
     public class DtoContent
     {
         public IEnumerable<IMarker> ArticleMarkerList { get; set; } 
-
         public IEnumerable<Article> ArticleList { get; set; } 
     }
 
