685f71b3dc4fd7122fd7f3df1f7f5400d329d5b9
Shaun Hevey <shaun@hevey.co>
2015-07-04 10:40:29 +1000

    Added IMarker and ArticleMarker
    Added IMarker and ArticleMarker

---
+++ GovHack2015/GovHack2015/Models/ArticleMarker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GovHack2015.Models
+{
+    public class ArticleMarker : IMarker
+    {
+        public string Title { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public string Symbol { get; set; }
+        
+    }
+}
---
+++ GovHack2015/GovHack2015/Models/IMarker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GovHack2015.Models
+{
+    public  interface IMarker
+    {
+        string Title { get; set; }
+        string Lat { get; set; }
+        string Lon { get; set; }
+        string Symbol { get; set; }
+    }
+}
