b0d7f296c42eb212e508532215d452f7eed9ee7e
MitchThors <mitch.thors@gmail.com>
2015-07-04 10:31:52 +1000

    Added more useful stuff!

---
+++ GovHack2015/GovHack2015/Global/GlobalData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace GovHack2015.Global
+{
+    public class GlobalData
+    {
+        
+        public void ConfigureApplication()
+        {
+        }
+        
+    }
+}
---
+++ GovHack2015/GovHack2015/Global/GlobalMessages.cs
@@ -0,0 +1,9 @@
+namespace GovHack2015.Global
+{
+    public static class GlobalMessages
+    {
+        public const string unknownError = "<b>Sorry. The system has made a boo-boo while trying to do its magic :(</b> Time to chillax and grab a coffee! A team " +
+                                           "of highly trained little code monkeys have been dispatched to deal with this situation. If you see them, tell them to " +
+                                           "contact support on the number below and check their logs. Thanks!";
+    }
+}
